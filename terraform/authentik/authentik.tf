a63c5d83e77cb009f002f799930c900cc3c4b5c2
Maksym Ivanchenko <github@ivanchenko.io>
2024-12-24 20:57:51 +0000

    add terraform/authentik

---
+++ terraform/authentik/authentik.tf
@@ -0,0 +1,33 @@
+data "authentik_flow" "default-authorization-flow" {
+  slug = "default-provider-authorization-implicit-consent"
+}
+
+data "authentik_flow" "default-invalidation-flow" {
+  slug = "default-invalidation-flow"
+}
+
+data "authentik_certificate_key_pair" "generated" {
+  name = "authentik Self-signed Certificate"
+}
+
+data "authentik_property_mapping_provider_scope" "scope-email" {
+  name = "authentik default OAuth Mapping: OpenID 'email'"
+}
+
+data "authentik_property_mapping_provider_scope" "scope-profile" {
+  name = "authentik default OAuth Mapping: OpenID 'profile'"
+}
+
+data "authentik_property_mapping_provider_scope" "scope-openid" {
+  name = "authentik default OAuth Mapping: OpenID 'openid'"
+}
+
+data "authentik_user" "akadmin" {
+  username = "akadmin"
+}
+
+resource "authentik_group" "admins" {
+  name         = "admins"
+  users = [data.authentik_user.akadmin.id]
+  is_superuser = true
+}
